2973df261d146cc188346c102225e763ceef30d5
FeelW1N <ubushaevv@gmail.com>
2024-12-07 20:27:24 +0300

    fixed env.py

--- TelegramBot/app/migration/env.py
+++ TelegramBot/app/migration/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 from dotenv import load_dotenv
 from alembic import context
 import os
-
+from models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 config.set_main_option('sqlalchemy.url', os.getenv('SQLALCHEMY_URL'))
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
